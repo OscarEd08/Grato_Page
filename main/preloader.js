2e0c59464334bef3d3e51ddac059a1e480ecd4a3
OscarEd08 <oscar.pairazaman2@unmsm.edu.pe>
2021-07-08 22:51:41 -0500

    cambios al loader v3

--- main/preloader.js
+++ main/preloader.js
@@ -1,12 +1,6 @@
 
 //LOADER
 
-/*$(window).on('load', function() { // makes sure the whole site is loaded 
-    $('animate').fadeOut(); // will first fade out the loading animation 
-    $('#preloader').delay(1800).fadeOut('slow'); // will fade out the white DIV that covers the website. 
-    $('body').delay(1800).css({'overflow':'visible'});
-})*/
-
 var container = document.getElementById('preloader');
 setTimeout(function(){
     
