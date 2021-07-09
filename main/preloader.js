d4fc53a3640243239680e00d0e24f9c833f7e2a3
OscarEd08 <oscar.pairazaman2@unmsm.edu.pe>
2021-07-08 22:32:45 -0500

    cambio de tiempo al loader

--- main/preloader.js
+++ main/preloader.js
@@ -1,9 +1,15 @@
 
 //LOADER
 
-$(window).on('load', function() { // makes sure the whole site is loaded 
+/*$(window).on('load', function() { // makes sure the whole site is loaded 
     $('animate').fadeOut(); // will first fade out the loading animation 
     $('#preloader').delay(1800).fadeOut('slow'); // will fade out the white DIV that covers the website. 
     $('body').delay(1800).css({'overflow':'visible'});
-})
+})*/
+
+var container = document.getElementById('preloader');
+setTimeout(function(){
+    container.classList.add('close');
+    $('#preloader').delay(500).fadeOut('slow');
+}, 1600);
 
